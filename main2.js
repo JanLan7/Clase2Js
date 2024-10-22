d61f0843797548871a6aa36bd5c6f626bcf7b32d
JanLan <idiomasjanlan@gmail.com>
2024-10-21 23:51:01 -0300

    clase 2 de javascript ejercicios

---
+++ main2.js
@@ -0,0 +1,7 @@
+const contraseña = "jazzdoit";
+
+let contraseñaIngresada = prompt("Ingresa la contraseña");
+while(contraseñaIngresada != contraseña){
+    contraseñaIngresada = prompt("Ingresa la contraseña");
+    
+}alert("Al fin, casi te olvidas tu contra 😅")
